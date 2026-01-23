bdc729b9dc4018ff89173efe1c0a09145a54b48a
xaizek <xaizek@posteo.net>
2026-01-23 12:16:48 +0200

    Use not_windows predicate in tests/ioeta/calculate

--- tests/ioeta/calculate.c
+++ tests/ioeta/calculate.c
@@ -2,6 +2,8 @@
 
 #include <stddef.h> /* NULL */
 
+#include <test-utils.h>
+
 #include "../../src/io/private/ioeta.h"
 #include "../../src/io/ioeta.h"
 #include "../../src/io/iop.h"
@@ -64,9 +66,7 @@ TEST(shallow_estimation_does_not_recur)
 	ioeta_free(estim);
 }
 
-#ifndef _WIN32
-
-TEST(symlink_calculated_as_zero_bytes)
+TEST(symlink_calculated_as_zero_bytes, IF(not_windows))
 {
 	ioeta_estim_t *const estim = ioeta_alloc(NULL, no_cancellation);
 
@@ -94,8 +94,6 @@ TEST(symlink_calculated_as_zero_bytes)
 
 	ioeta_free(estim);
 }
-
-#endif
 
 /* vim: set tabstop=2 softtabstop=2 shiftwidth=2 noexpandtab cinoptions-=(0 : */
 /* vim: set cinoptions+=t0 filetype=c : */
